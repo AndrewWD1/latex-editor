e78138d92b31d407370f94c63a4357a8a08ddb51
Andrew Doumont <andrew.doumont@gmail.com>
2019-10-13 15:21:18 +0900

    udpate

--- src/components/viewer/viewer.component.jsx
+++ src/components/viewer/viewer.component.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { selectPDFLink } from "../../redux/files/files.selectors";
 
 const Viewer = ({ width, height, pdfLink }) => {
   return (
-    <iframe id="PDF" title="PDF" width={width} height={height} src={pdfLink} />
+    <iframe
+      allow-same-origin
+      id="PDF"
+      title="PDF"
+      width={width}
+      height={height}
+      src={""}
+    />
   );
 };
 

a18ea1597ae8e4044fa353c19f2b3dd82d42f3cc
Andrew Doumont <andrew.doumont@gmail.com>
2019-11-06 13:02:53 +0900

    udpate

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./App";
 
-/**
- * Creating a Web app to write and view Latex files.
- * TODO: Create User-Profile for Front-End
- * ! Create Back end on heroku that will compile pdf and send back to Front-End
- */
-
 render(
   <Provider store={store}>
     <App />

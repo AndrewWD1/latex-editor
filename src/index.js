8d8f6174ddfc62ed11178f63f3adb215ec682bad
Andrew Doumont <andrew.doumont@gmail.com>
2020-05-01 04:29:43 -0600

    update

--- src/index.js
+++ src/index.js
@@ -5,17 +5,15 @@ import { store } from "./redux/store";
 import App from "./App";
 
 render(
-  <Provider store={store}>
-    <div
-      style={{
-        color: "white",
-        textAlign: "center",
-        fontSize: "2rem",
-        margin: "2rem",
-      }}
-    >
-      This site has been taken down for the time being.
-    </div>
-  </Provider>,
+  <div
+    style={{
+      color: "white",
+      textAlign: "center",
+      fontSize: "2rem",
+      margin: "2rem",
+    }}
+  >
+    This site has been taken down for the time being.
+  </div>,
   document.getElementById("root")
 );
